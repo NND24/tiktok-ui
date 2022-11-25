a182c328c83a61f73a46d424c2d3a792c112c534
Dat Nguyen <ngocdatnguyen2404@gmail.com>
2022-11-25 21:42:20 +0700

    Sửa lỗi và hoàn thiện phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -5,9 +5,6 @@ import {
   faEarthAsia,
   faCircleQuestion,
   faKeyboard,
-  faCloudDownload,
-  faPaperPlane,
-  faMessage,
   faUser,
   faCoins,
   faGear,
@@ -22,6 +19,8 @@ import 'tippy.js/dist/tippy.css';
 import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
 import Image from '~/components/Image';
 import Search from '../Search';
+import { Link } from 'react-router-dom';
+import routesConfig from '~/config/routes';
 
 const cx = classNames.bind(styles);
 
@@ -88,7 +87,9 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        <img src={images.logo} alt="Tiktok" />
+        <Link to={routesConfig.home} className={cx('logo-link')}>
+          <img src={images.logo} alt="Tiktok" />
+        </Link>
 
         <Search />
 
--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -31,9 +31,13 @@ function Search() {
 
     const fetchApi = async () => {
       setLoading(true);
+
       const result = await searchServices.search(debounced);
+
       setSearchResult(result);
+      setLoading(false);
     };
+
     fetchApi();
   }, [debounced]);
 
@@ -45,6 +49,13 @@ function Search() {
 
   const handleHideResult = () => {
     setShowResult(false);
+  };
+
+  const handleChange = (e) => {
+    const searchValue = e.target.value;
+    if (!searchValue.startsWith(' ')) {
+      setSearchValue(searchValue);
+    }
   };
 
   return (
@@ -69,7 +80,7 @@ function Search() {
           value={searchValue}
           placeholder="Tìm kiếm tài khoản và video"
           spellCheck="false"
-          onChange={(e) => setSearchValue(e.target.value)}
+          onChange={handleChange}
           onFocus={() => setShowResult(true)}
         />
         {!!searchValue && !loading && (
@@ -79,7 +90,12 @@ function Search() {
         )}
         {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
 
-        <button className={cx('search-btn')}>
+        <button
+          className={cx('search-btn')}
+          onMouseDown={(e) => {
+            e.preventDefault();
+          }}
+        >
           <SearchIcon />
         </button>
       </div>
